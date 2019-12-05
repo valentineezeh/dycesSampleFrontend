13fcc2d310e136d37ff363780c9ed32f230668b4
Valentine <val.ezeh15@gmail.com>
2019-12-05 16:34:19 +0100

    change react image

--- src/components/unauthenticated.components/LandingPage.jsx
+++ src/components/unauthenticated.components/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
+import constructionImg from '../../template/utils/images/hammer.png';
 
 /**
  * @class LandingPage
@@ -13,10 +14,23 @@ class LandingPage extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <img
+            src={constructionImg}
+            className="App-logo"
+            style={{ height: '40vmin'}}
+            alt="logo"
+
+           />
+          <p style={{ fontFamily: 'DM Sans' }}>
           Work in progress.
+            <br />
+            <strong
+              style={{ fontSize: '20px' }}
+            >
+            It is almost ready... honest
+            </strong>
           </p>
+
           <a
             className="App-link"
           >
